818f08cd9dd766d278db0bf43a7eed2035e373ad
huntojkojqw01 <hungnd.k58@gmail.com>
2017-12-14 23:07:56 +0700

    fix bug mem buffer van luu du lieu tu lan ghi truoc, lam dep code, tach han ra hai device rieng

--- Makefile
+++ Makefile
@@ -4,7 +4,6 @@ KDIR = /lib/modules/$(shell uname -r)/build
 
 all:
 	$(MAKE) -C $(KDIR) SUBDIRS=$(PWD) modules
-	gcc -o main main.c rsa.c
-	gcc -o test test_init_rsa.c rsa.c
+	gcc -o main main.c rsa.c	
 clean:  
-	rm -rf *.o *.ko *.mod.* *.symvers *.order  .*.*.cmd main encode decode 
+	rm -rf *.o *.ko *.mod.* *.symvers *.order  .*.*.cmd main
--- main.c
+++ main.c
@@ -3,73 +3,96 @@
 #include <string.h>
 #include <stdlib.h>
 #include "rsa.h"
+#define RED     "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define YELLOW  "\x1b[33m"
+#define BLUE    "\x1b[34m"
+#define MAGENTA "\x1b[35m"
+#define CYAN    "\x1b[36m"
+#define RESET   "\x1b[0m"
 
 int main(int argc,char* argv[]) { 
 
-  unsigned char dummy;
-  char *write_str,*read_str;
+  unsigned char dummy, i;
+  char *write_str, *read_str, src[1000], des[1000];;
 
   FILE *ENCODER, *DECODER;
   rsa_params _rsa;
-  init(&_rsa);
 
-  // char src[1000],des[1000]; 
-  // if(argc>=2){    
-  //   strcpy(src,argv[1]);
-  //   ma_hoa2(_rsa,src,des);
-  //   puts(src);puts(des);
-  //   strcpy(src,des);
-  //   giai_ma2(_rsa,src,des);
-  //   puts(src);puts(des);
-  // }
+  if(argc >= 2){
+    if(strcmp(argv[1], "-i") == 0){
+      init(&_rsa);
+      printf("Init RSA attributes: "RED"n=%d, e=%d, d=%d, u=%d"RESET"\n",_rsa.n, _rsa.e, _rsa.d, _rsa.u);
+      return 1;
+    }
+    if(strcmp(argv[1], "-t") == 0){
+      init(&_rsa);
+      puts("Test RSA algorithms...");
+      if(argc>=3)    strcpy(src, argv[2]);
+      else           strcpy(src,"default string");
+      ma_hoa2(_rsa, src, des);
+      printf("   Encode: "GREEN"\"%s\""RESET" ----> "RED"\"%s\""RESET" \n",src,des);
+      strcpy(src,des);
+      giai_ma2(_rsa, src, des);
+      printf("   Decode: "GREEN"\"%s\""RESET" <---- "RED"\"%s\""RESET" \n",des,src);
+      return 1;
+    }  
 
-  if(argc<2){
+    puts("Test encoder device and decoder device...");
+    /* Opening the device memory */
+
+    ENCODER=fopen("/dev/encoder", "w+");
+    DECODER=fopen("/dev/decoder", "w+");
+    if(!ENCODER || !DECODER){
+      puts(RED"IO ERROR!!!"RESET" (Device not found).");
+      return -1;
+    }
+
+    /* We remove the buffer from the file i/o */
+    setvbuf(ENCODER, &dummy, _IONBF,BUFF_SIZE);
+    setvbuf(DECODER, &dummy, _IONBF,BUFF_SIZE);
+
+    // Prepare buffer:
+    write_str=(char*)malloc(sizeof(char)*BUFF_SIZE);
+    memset(write_str, 0, BUFF_SIZE);
+    read_str=(char*)malloc(sizeof(char)*BUFF_SIZE);
+    memset(read_str, 0, BUFF_SIZE);
+
+    //USE ENCODER:
+    strcpy(write_str,argv[1]);
+
+    printf("   Writing "GREEN"\"%s\""RESET" to encoder device... \n",write_str);
+    fwrite(write_str, 1, strlen(write_str),ENCODER);
+
+    puts("   ..."); sleep(1);  
+
+    fread(read_str,sizeof(char),BUFF_SIZE,ENCODER);
+    printf("   Read from encoder device, get "RED"\"%s\""RESET"\n",read_str);
+
+    for(i = 0; i<3; i++){
+      puts("   ..."); sleep(1); 
+    }
+    // USE DECODER:
+    strcpy(write_str,read_str);
+
+    printf("   Writing "RED"\"%s\""RESET" to decoder device... \n",write_str);
+    fwrite(write_str, 1, strlen(write_str),DECODER);
+
+    puts("   ..."); sleep(1);  
+
+    fread(read_str,sizeof(char),BUFF_SIZE,DECODER);
+    printf("   Read from decoder device, get "GREEN"\"%s\""RESET"\n",read_str);
+
+    fclose(ENCODER);
+    fclose(DECODER);
+    return 1;
+  }
+  else{
     puts("Argument not found.");
+    puts("./main -i #init RSA attributes");
+    puts("./main -t [string] #test RSA with input string");
+    puts("./main string #test encoder device and decoder device ");
     return -1;
   }  
-
-  /* Opening the device memory */
-
-  ENCODER=fopen("/dev/encoder","w+");
-  DECODER=fopen("/dev/decoder","w+");
-  if(!ENCODER || !DECODER){
-    puts("IO ERROR!!! (Device not found).");
-    return -1;
-  }
-
-  /* We remove the buffer from the file i/o */
-  setvbuf(ENCODER,&dummy,_IONBF,BUFF_SIZE);
-  setvbuf(DECODER,&dummy,_IONBF,BUFF_SIZE);
-
-  // Prepare buffer:
-  write_str=(char*)malloc(sizeof(char)*BUFF_SIZE);
-  memset(write_str,0,BUFF_SIZE);
-  read_str=(char*)malloc(sizeof(char)*BUFF_SIZE);
-  memset(read_str,0,BUFF_SIZE);
-
-  //USE ENCODER:
-  strcpy(write_str,argv[1]);
-
-  printf("Writing \"%s\" to encoder device... \n",write_str);
-  fwrite(write_str,1,strlen(write_str),ENCODER);
-
-  sleep(1);  
-
-  fread(read_str,sizeof(char),BUFF_SIZE,ENCODER);
-  printf("Read from encoder device, get \"%s\"\n",read_str);
-
-  // USE DECODER:
-  strcpy(write_str,read_str);
-
-  printf("Writing \"%s\" to decoder device... \n",write_str);
-  fwrite(write_str,1,strlen(write_str),DECODER);
-
-  sleep(1);  
-
-  fread(read_str,sizeof(char),BUFF_SIZE,DECODER);
-  printf("Read from decoder device, get \"%s\"\n",read_str);
-
-  fclose(ENCODER);
-  fclose(DECODER);
   return 0;
 }
